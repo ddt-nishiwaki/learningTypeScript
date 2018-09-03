61817a240ab07bf4a3575c515df976812686281a
SeijiNishiwaki <nishiwakiat.wjnisi@gmail.com>
2018-09-03 13:45:21 +0900

    【課題】null非許容設定でのundefined,nullの利用

--- src/checkStrictNullCheck.ts
+++ src/checkStrictNullCheck.ts
@@ -1,6 +1,7 @@
 /**
  * TypeScriptはデフォルトではどの型でも undefined,nullの代入を許容します。
  * バグの原因にもなりやすいのでTypeScriptの設定で許容しないようにできます。
+ * この設定で null, undefined を設定したい場合は明示的に型を指定します。
  * 
  * コンパイルコマンド
  * 
@@ -21,12 +22,13 @@
  *         null
  * 
  *     undefined, null を許容しない場合
- *         コンパイルエラー
+ *         undefined
+ *         null
  */
 // string型の変数に undefinedの代入を試みる
-let undefined_data: string = undefined;
+let undefined_data: string | undefined = undefined;
 // string型の変数に nullの代入を試みる
-let null_data: string = null;
+let null_data: string | null = null;
 
 // undefinedが代入されていることを確認するため出力する
 console.log(undefined_data);
